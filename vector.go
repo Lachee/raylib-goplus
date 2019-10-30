871990f708db5ad7efeec6ab0df09f6949f2e9fe
Lachee <lachlan.h@hotmail.com>
2019-10-30 23:25:55 +1100

    Finished Vector Added Initial Quaternion

--- vector.go
+++ vector.go
@@ -47,6 +47,21 @@ func (v Vector2) DotProduct(v2 Vector2) float32 {
 	return v.X*v2.X + v.Y*v2.Y
 }
 
+//Perpendicular to this vector
+func (v Vector2) Perpendicular() Vector2 {
+	return Vector2{X: v.Y, Y: v.X}
+}
+
+//RotateByRadians rotates the vector in radians. Use Deg2Rad to convert degress into radians.
+func (v Vector2) RotateByRadians(radians float32) Vector2 {
+	sin := float32(math.Sin(float64(radians)))
+	cos := float32(math.Cos(float64(radians)))
+	return Vector2{
+		X: (cos * v.X) - (sin * v.Y),
+		Y: (sin * v.X) + (cos * v.Y),
+	}
+}
+
 //Angle the vector creates with another vector
 func (v Vector2) Angle(v2 Vector2) float32 {
 	result := float32(math.Atan2(float64(v2.Y-v.Y), float64(v2.X-v.X))) * Rad2Deg
@@ -103,6 +118,22 @@ func (v Vector2) Distance(v2 Vector2) float32 {
 //Reflect a vector. The mirror normal can be invisioned as a mirror perpendicular to the surface that is hit.
 func (v Vector2) Reflect(mirrorNormal Vector2) Vector2 {
 	return v.Add(v.Scale(-2 * v.DotProduct(mirrorNormal)))
+}
+
+//Min value for each pair of components
+func (v Vector2) Min(v2 Vector2) Vector2 {
+	return Vector2{
+		X: float32(math.Min(float64(v.X), float64(v2.X))),
+		Y: float32(math.Min(float64(v.Y), float64(v2.Y))),
+	}
+}
+
+//Max value for each pair of components
+func (v Vector2) Max(v2 Vector2) Vector2 {
+	return Vector2{
+		X: float32(math.Max(float64(v.X), float64(v2.X))),
+		Y: float32(math.Max(float64(v.Y), float64(v2.Y))),
+	}
 }
 
 //Vector3 a 3 dimensional vector
@@ -271,8 +302,8 @@ func (v Vector3) Transform(m Matrix) Vector3 {
 func (v Vector3) Min(v2 Vector3) Vector3 {
 	return Vector3{
 		X: float32(math.Min(float64(v.X), float64(v2.X))),
-		Y: float32(math.Min(float64(v.Y), float64(v2.Z))),
-		Z: float32(math.Min(float64(v.Y), float64(v2.Z))),
+		Y: float32(math.Min(float64(v.Y), float64(v2.Y))),
+		Z: float32(math.Min(float64(v.Z), float64(v2.Z))),
 	}
 }
 
@@ -280,9 +311,14 @@ func (v Vector3) Min(v2 Vector3) Vector3 {
 func (v Vector3) Max(v2 Vector3) Vector3 {
 	return Vector3{
 		X: float32(math.Max(float64(v.X), float64(v2.X))),
-		Y: float32(math.Max(float64(v.Y), float64(v2.Z))),
-		Z: float32(math.Max(float64(v.Y), float64(v2.Z))),
+		Y: float32(math.Max(float64(v.Y), float64(v2.Y))),
+		Z: float32(math.Max(float64(v.Z), float64(v2.Z))),
 	}
+}
+
+//QuaternionToo calculates the Quaternion between the current vector and the next
+func (v Vector3) QuaternionToo(v2 Vector3) Quaternion {
+	return newQuaternionVector3ToVector3(v, v2)
 }
 
 //Barycenter computers the coordinates (u, v, w) for the vector with respect to triangle (a, b, c). Assumes vector is on plane with triangle
@@ -312,14 +348,107 @@ type Vector4 struct {
 	W float32
 }
 
-//Quaternion are used to represent rotation without gimbal lock
-type Quaternion struct {
-	X float32
-	Y float32
-	Z float32
-	W float32
+//Decompose the vector into a slice of floats
+func (v Vector4) Decompose() []float32 { return []float32{v.X, v.Y, v.Z, v.W} }
+
+//Add two vectors (v1 + v2)
+func (v Vector4) Add(v2 Vector4) Vector4 {
+	return Vector4{X: v.X + v2.X, Y: v.Y + v2.Y, Z: v.Z + v2.Z, W: v.W + v2.W}
 }
 
+//Subtract two vectors (v1 - v2)
+func (v Vector4) Subtract(v2 Vector4) Vector4 {
+	return Vector4{X: v.X - v2.X, Y: v.Y - v2.Y, Z: v.Z - v2.Z, W: v.W - v2.W}
+}
+
+//Scale the vector (v * scale)
+func (v Vector4) Scale(scale float32) Vector4 {
+	return Vector4{X: v.X * scale, Y: v.Y * scale, Z: v.Z * scale, W: v.W * scale}
+}
+
+//Multiply a vector by another vector
+func (v Vector4) Multiply(v2 Vector4) Vector4 {
+	return Vector4{X: v.X * v2.Y, Y: v.Y * v2.Y, Z: v.Z * v2.Z, W: v.W * v2.W}
+}
+
+//Divide  a vector by a value (v / d)
+func (v Vector4) Divide(d float32) Vector4 {
+	return Vector4{X: v.X / d, Y: v.Y / d, Z: v.Z / d, W: v.W / d}
+}
+
+//DivideV a vector by another vecotr (v / v2)
+func (v Vector4) DivideV(v2 Vector4) Vector4 {
+	return Vector4{X: v.X / v2.Y, Y: v.Y / v2.Y, Z: v.Z / v2.Z, W: v.W / v2.W}
+}
+
+//Negate or Inverts a vector
+func (v Vector4) Negate() Vector4 {
+	return Vector4{X: -v.X, Y: -v.Y, Z: -v.Z, W: -v.W}
+}
+
+//Length of the vector
+func (v Vector4) Length() float32 {
+	return float32(math.Sqrt(float64(v.X*v.X) + float64(v.Y*v.Y) + float64(v.Z*v.Z) + float64(v.W*v.W)))
+}
+
+//SqrLength is the squared length of the vector
+func (v Vector4) SqrLength() float32 {
+	return float32(float64(v.X*v.X) + float64(v.Y*v.Y) + float64(v.Z*v.Z) + float64(v.W*v.W))
+}
+
+//DotProduct of the vector
+func (v Vector4) DotProduct(v2 Vector4) float32 {
+	return v.X*v2.X + v.Y*v2.Y + v.Z*v2.Z + v.W*v2.W
+}
+
+//Normalize a vector
+func (v Vector4) Normalize() Vector4 {
+	length := v.Length()
+	if length == 0 {
+		length = 1
+	}
+
+	ilength := 1 / length
+	return v.Scale(ilength)
+}
+
+//Distance between two vectors
+func (v Vector4) Distance(v2 Vector4) float32 {
+	d := v2.Subtract(v)
+	return d.Length()
+}
+
+//Lerp a vector towards another vector
+func (v Vector4) Lerp(target Vector4, amount float32) Vector4 {
+	return Vector4{
+		X: v.X + amount*(target.X-v.X),
+		Y: v.Y + amount*(target.Y-v.Y),
+		Z: v.Z + amount*(target.Z-v.Z),
+		W: v.W + amount*(target.W-v.W),
+	}
+}
+
+//Min value for each pair of components
+func (v Vector4) Min(v2 Vector4) Vector4 {
+	return Vector4{
+		X: float32(math.Min(float64(v.X), float64(v2.X))),
+		Y: float32(math.Min(float64(v.Y), float64(v2.Y))),
+		Z: float32(math.Min(float64(v.Z), float64(v2.Z))),
+		W: float32(math.Min(float64(v.W), float64(v2.W))),
+	}
+}
+
+//Max value for each pair of components
+func (v Vector4) Max(v2 Vector4) Vector4 {
+	return Vector4{
+		X: float32(math.Max(float64(v.X), float64(v2.X))),
+		Y: float32(math.Max(float64(v.Y), float64(v2.Y))),
+		Z: float32(math.Max(float64(v.Z), float64(v2.Z))),
+		W: float32(math.Max(float64(v.W), float64(v2.W))),
+	}
+}
+
+//Matrix A representation of a 4 x 4 matrix
 type Matrix struct {
 	M0  float32
 	M1  float32
@@ -338,6 +467,9 @@ type Matrix struct {
 	M14 float32
 	M15 float32
 }
+
+func newMatrixFromQuaternion(q Quaternion) { return nil }
+func (m Matrix) ToQuaternion() Quaternion  { return newQuaternionFromMatrix(m) }
 
 //Clamp a value between min and max
 func Clamp(v float64, min float64, max float64) float64 {
