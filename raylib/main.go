a942b3b6a0a07d14b2914d20b3641b466b4f54e1
Lachee <lachlan.h@hotmail.com>
2019-10-31 16:49:22 +1100

    Added back all the headers

--- raylib/main.go
+++ raylib/main.go
@@ -1,12 +1,33 @@
 package raylib
 
-//
-
 /*
-#cgo CFLAGS: -std=gnu99 -Wno-missing-braces -Wno-unused-result
+#cgo android LDFLAGS: -llog -landroid -lEGL -lGLESv2 -lOpenSLES -lm
+#cgo android CFLAGS: -DPLATFORM_ANDROID -DGRAPHICS_API_OPENGL_ES2 -DMAL_NO_SDL -Iexternal/android/native_app_glue
 
-#cgo windows CFLAGS: -Iexternal -Iexternal/glfw/include -Iexternal/glfw/deps/mingw -DPLATFORM_DESKTOP -DRAYGUI_IMPLEMENTATION
+#cgo darwin LDFLAGS: -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo -framework CoreFoundation
+#cgo darwin CFLAGS: -x objective-c -Iexternal/glfw/include -D_GLFW_COCOA -D_GLFW_USE_CHDIR -D_GLFW_USE_MENUBAR -D_GLFW_USE_RETINA -Wno-deprecated-declarations -DPLATFORM_DESKTOP -DMAL_NO_COREAUDIO
+
+#cgo darwin,opengl11 CFLAGS: -DGRAPHICS_API_OPENGL_11
+#cgo darwin,opengl21 CFLAGS: -DGRAPHICS_API_OPENGL_21
+#cgo darwin,!opengl11,!opengl21 CFLAGS: -DGRAPHICS_API_OPENGL_33
+
+#cgo linux,arm LDFLAGS: -L/opt/vc/lib -L/opt/vc/lib64 -lbrcmGLESv2 -lbrcmEGL -lpthread -lrt -lm -lbcm_host -lvcos -lvchiq_arm -ldl
+#cgo linux,arm CFLAGS: -DPLATFORM_RPI -DGRAPHICS_API_OPENGL_ES2 -I/opt/vc/include -I/opt/vc/include/interface/vcos -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads
+
+#cgo linux CFLAGS: -Iexternal/glfw/include -DPLATFORM_DESKTOP -Wno-stringop-overflow
+
+#cgo linux,!wayland LDFLAGS: -lGL -lm -pthread -ldl -lrt -lX11
+#cgo linux,wayland LDFLAGS: -lGL -lm -pthread -ldl -lrt -lwayland-client -lwayland-cursor -lwayland-egl -lxkbcommon
+
+#cgo linux,!wayland CFLAGS: -D_GLFW_X11
+#cgo linux,wayland CFLAGS: -D_GLFW_WAYLAND
+
+#cgo linux,opengl11 CFLAGS: -DGRAPHICS_API_OPENGL_11
+#cgo linux,opengl21 CFLAGS: -DGRAPHICS_API_OPENGL_21
+#cgo linux,!opengl11,!opengl21 CFLAGS: -DGRAPHICS_API_OPENGL_33
+
 #cgo windows LDFLAGS: -lopengl32 -lgdi32 -lwinmm -lole32
+#cgo windows CFLAGS: -D_GLFW_WIN32 -Iexternal -Iexternal/glfw/include -Iexternal/glfw/deps/mingw -DPLATFORM_DESKTOP -DRAYGUI_IMPLEMENTATION
 
 #cgo windows,opengl11 CFLAGS: -DGRAPHICS_API_OPENGL_11
 #cgo windows,opengl21 CFLAGS: -DGRAPHICS_API_OPENGL_21
@@ -54,7 +75,7 @@ const (
 
 func init() {
 	//We have to lock the OS Thread as raylib is sensitive to that stuff.
-	runtime.LockOSThread()
+	//runtime.LockOSThread()
 }
 
 // SetConfigFlags - Setup some window configuration flags
@@ -80,11 +101,13 @@ func InitWindow(width int32, height int32, title string) {
 }
 
 func BeginDrawing() {
+	runtime.LockOSThread()
 	C.BeginDrawing()
 }
 
 func EndDrawing() {
 	C.EndDrawing()
+	runtime.UnlockOSThread()
 }
 
 func ClearBackground(color Color) {
