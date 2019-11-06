fe437e4c522b59fb881895ea0ff1208b1bffe2ea
Lachee <lachlan.h@hotmail.com>
2019-11-06 12:17:43 +1100

    Added structures

---
+++ raylib/audio.go
@@ -0,0 +1,65 @@
+package raylib
+
+/*
+#include "raylib.h"
+#include <stdlib.h>
+*/
+import "C"
+import "unsafe"
+
+//Wave defines audio wave data
+type Wave struct {
+	SampleCount uint32
+	SampleRate  uint32
+	SampleSize  uint
+	Channels    uint
+	data        unsafe.Pointer
+}
+
+func newWaveFromPointer(ptr unsafe.Pointer) Wave {
+	return *(*Wave)(ptr)
+}
+
+func (w *Wave) cptr() *C.Wave {
+	return (*C.Wave)(unsafe.Pointer(w))
+}
+
+//Sound source type
+type Sound struct {
+	SampleCount uint32
+	Stream      AudioStream
+}
+
+func newSoundFromPointer(ptr unsafe.Pointer) Sound {
+	return *(*Sound)(ptr)
+}
+
+func (s *Sound) cptr() *C.Sound {
+	return (*C.Sound)(unsafe.Pointer(s))
+}
+
+//AudioStream can be used to create custom audio streams.
+//Note that Buffer is an unsafe.Pointer and is a C stream.
+type AudioStream struct {
+	SampleRate uint32
+	SampleSize uint
+	Channels   uint
+	Buffer     unsafe.Pointer
+}
+
+func newAudioStreamFromPointer(ptr unsafe.Pointer) AudioStream {
+	return *(*AudioStream)(ptr)
+}
+
+func (as *AudioStream) cptr() *C.AudioStream {
+	return (*C.AudioStream)(unsafe.Pointer(as))
+}
+
+//Music stream type. Anything longer than ~10 seconds should be streamed.
+type Music struct {
+	CtxType     int32
+	CtxData     unsafe.Pointer
+	SampleCount uint32
+	LoopCount   uint32
+	Stream      AudioStream
+}
