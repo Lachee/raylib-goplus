a7082516575b0be0f9a9d0606d8bbbde5d07c5f5
Lachee <lachlan.h@hotmail.com>
2019-11-07 19:29:24 +1100

    Build script now tells if it was succesful.

--- build.sh
+++ build.sh
@@ -5,16 +5,19 @@ go run main.go
 echo "======= Copying Generated Files"
 echo "GUI";  	cp out/raygui_gen.go ../raylib/raygui_gen.go
 echo "Audio";  	cp out/audio_gen.go ../raylib/audio_gen.go
-echo "Camera";  cp out/camera_gen.go ../raylib/camera_gen.go
+echo "Texture";  cp out/texture_gen.go ../raylib/texture_gen.go
+#echo "Camera";  cp out/camera_gen.go ../raylib/camera_gen.go
 
 
 echo "======= Building Library"
 echo " ( this might take a while, please wait ) "
 cd ../raylib
-go build .
 
-echo "======= Reporting Lint Issues"
-golint .
-
-
-echo "======= Finished"
+if [[ $(go build .) ]];
+then
+	echo "======= Finished Unsucesfully"
+else
+	echo "======= Reporting Lint Issues"
+	golint .
+	echo "======= Finished Succesfully"
+fi
