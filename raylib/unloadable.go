241367241d14558c058f7929496e35cbc2fd53c8
Lachee <lachlan.h@hotmail.com>
2019-11-11 12:15:06 +1100

    Fixed unloadables

--- raylib/unloadable.go
+++ raylib/unloadable.go
@@ -11,14 +11,14 @@ var unloadables []Unloadable = make([]Unloadable, 100)
 
 //TODO: Fix this
 func finalizeUnloadables(unlds *[]Unloadable) {
-	TraceLog(LogInfo, "Finalizing Unloadables")
+	TraceLog(LogInfo, "[UNLOAD] Finalizing Unloadables")
 	UnloadAll()
 }
 
 //addUnloadable registers an unloadable to the slice
 // This is called on Load functions
 func addUnloadable(unloadable Unloadable) {
-	TraceLog(LogTrace, "New unloadable created")
+	TraceLog(LogTrace, "[UNLOAD] New unloadable created")
 	unloadables = append(unloadables, unloadable)
 }
 
@@ -27,10 +27,13 @@ func addUnloadable(unloadable Unloadable) {
 // This does not remove from the slice if unloadingAll is true (as that will clear post)
 func removeUnloadable(unloadable Unloadable) {
 	if !unloadingAll {
-		TraceLog(LogTrace, "Remocing unloadable")
+		TraceLog(LogTrace, "[UNLOAD] Removing unloadable")
 		for i, u := range unloadables {
 			if u == unloadable {
-				copy(unloadables[i:], unloadables[i+1:])
+				unloadables[i] = unloadables[len(unloadables)-1]
+				unloadables[len(a)-1] = nil
+				unloadables = unloadables[:len(unloadables)-1]
+				TraceLog(LogTrace, "[UNLOAD] ---- REMOVED")
 				break
 			}
 		}
@@ -41,17 +44,22 @@ func removeUnloadable(unloadable Unloadable) {
 // NOTE: Not everything maybe included in this list and it is experimental feature.
 // 			 Please unload these objects when you are not using them anyways.
 func UnloadAll() {
-	TraceLog(LogInfo, "Unloading all unloadables")
+	TraceLog(LogInfo, "[UNLOAD] Unloading all unloadables: ", len(unloadables))
+
+	//Count the tally
+	tally := 0
 
 	//Unload everything
 	unloadingAll = true
 	for _, ul := range unloadables {
 		if ul != nil {
 			ul.Unload()
+			tally++
 		}
 	}
 	unloadingAll = false
 
 	//Clear the unloadables
 	unloadables = unloadables[:0]
+	TraceLog(LogInfo, "[UNLOAD] Unloaded ", tally)
 }
